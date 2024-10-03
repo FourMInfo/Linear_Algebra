c47ab365b93ff28199d798895bf0bb55f51d1e25
Aron T <aront@fourm.info>
2024-10-03 17:33:56 +0300

    ritation matrix

--- src/linear_algebra_transform.jl
+++ src/linear_algebra_transform.jl
@@ -7,7 +7,8 @@ Create symbolic matrix for projection on [u, v] using E₁ = [1,0] and E₂ =[0,
 """
 function projection_matrix_symbolic()
     @variables u v
-    [orthproj([u, v], [1, 0])[1] orthproj([u, v], [0, 1])[1]; orthproj([u, v], [1, 0])[2] orthproj([u, v], [0, 1])[2]]
+#    [orthproj([u, v], [1, 0])[1] orthproj([u, v], [0, 1])[1]; orthproj([u, v], [1, 0])[2] orthproj([u, v], [0, 1])[2]]
+    [permutedims(orthproj([u,v], [1,0])); permutedims(orthproj([u,v], [0,1]))]
 end
 """
     function projection_matrix_symbolic_polar() -> Matrix
@@ -27,9 +28,41 @@ function projection_matrix(x::Vector)
 end
 """
     function projection_matrix_polar(θ::Number) -> Matrix
-value of Matrix with an actual value for the angle of the vector
+value of projection polar matrix with an actual value for the angle of the vector
 """
 function projection_matrix_polar(n::Number)
     @variables u v θ
     Symbolics.value.(substitute.(projection_matrix_symbolic_polar(), θ => deg2rad(n),))
+end
+
+"""
+    function rotation_matrix_symbolic() -> Matrix
+Create symbolic matrix for rotation θ degrees,  using E₁ = [1,0] and E₂ =[0,1] to get first and second row
+"""
+function rotation_matrix_symbolic()
+    @variables θ
+    # calculate for e₁ = [1, 0]
+    x′ = (cos(θ) * 1) - (sin(θ) * 0)
+    y′ = (sin(θ) * 1) + (cos(θ) * 0)
+    # calculate for e₂ = [0, 1]
+    x′′ = (cos(θ) * 0) - (sin(θ) * 1)
+    y′′ = (sin(θ) * 0) + (cos(θ) * 1)
+    [x′ x′′; y′ y′′]  
+end
+
+"""
+    function rotation_matrix(d::Number) -> Matrix
+value of rotation Matrix with an actual value for the rotation angle
+"""
+function rotation_matrix(d::Number)
+    @variables θ
+    Symbolics.value.(substitute.(rotation_matrix_symbolic(), θ => deg2rad(d),))
+end
+
+"""
+    function rotation_matrix_ns(θ::Number) -> Matrix
+value of rotation Matrix with an actual value for the rotation angle - non symbolic version
+"""
+function rotation_matrix_ns(θ::Number)
+    [rotation(θ, e₁) rotation(θ, e₂)]
 end
